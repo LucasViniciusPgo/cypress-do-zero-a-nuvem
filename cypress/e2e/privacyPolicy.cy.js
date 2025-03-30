e8deaffb115c563b72c6ebfc4a1325e531cf4794
Lucas Vinicius Pereira Gonçalves de Oliveira <lucasvinicius.pgo@outlook.com>
2025-03-30 18:03:09 -0300

    adjust privacy.html

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -2,8 +2,7 @@ it('testa a página da política de privacidade de forma independente', () => {
 
     cy.visit('./src/privacy.html')
 
-    cy.origin('./src/privacy.html', ()=>{
       cy.title().should('eq', 'Central de Atendimento ao Cliente TAT - Política de Privacidade')
-    })
+   
 
   })
